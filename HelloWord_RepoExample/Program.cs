cb2863e82b4a5ba3ea2772c3cbcfcea0b4eefb99
carrieah14 <69916469+carrieah14@users.noreply.github.com>
2021-01-19 09:10:41 -0500

    Add project files.

---
+++ HelloWord_RepoExample/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace HelloWord_RepoExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
